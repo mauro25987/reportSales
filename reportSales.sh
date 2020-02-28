d414cffb3aa4868c12356963c89348e029c6b3cf
Mauro Gonzalez <maurogonzalezbruno@gmail.com>
2020-02-28 12:55:56 -0300

    add total login vs sales

--- reportSales.sh
+++ reportSales.sh
@@ -21,7 +21,7 @@ echo "Host: $host"
 
 #necesitmaos ajustar la fecha de entry_date por que actualmente los leads que se ingresan por la api quedan con GMT-0 y el resto de los datos del server estan en GMT-5
 
-sql1_func(){
+sql_func(){
 	if [ -z $interval ]; then
 		sql1="select count(*) from vicidial_list where list_id='$list' and entry_date >= '$fechaI' 
 			and entry_date < '$fechaF';"
@@ -30,46 +30,46 @@ sql1_func(){
 		sql1="select count(*) from vicidial_list where list_id='$list' and entry_date >= date_add('$fechaI',interval $interval hour) and 
 			entry_date < date_add('$fechaF',interval $interval hour);"
 	fi
-	}
 
-sql2_func(){
 	sql2="select count(*) from vicidial_list where list_id='$list' 
 		and modify_date >= '$fechaI' and modify_date < '$fechaF' and status in ( $statusSales );"	
-	}
 
-sql3_func(){
-	sql3="select count(*) from vicidial_list where list_id='$list' and ( modify_date >= '$fechaI%' and modify_date < '$fechaF' 
+	sql3="select count(*) from vicidial_list where list_id='$list' and ( modify_date >= '$fechaI' and modify_date < '$fechaF' 
 		and status in ( $statusContacts ));"	
+
+	sql4="select sum(wait_sec + talk_sec + dispo_sec + pause_sec) from vicidial_agent_log where campaign_id='$campaign' and event_time >= '$fechaI' and event_time < '$fechaF';"
 	}
 
-sql4="select list_id from vicidial_lists where campaign_id='$campaign' and active='Y';"
-sql5="select campaign_name from vicidial_campaigns where campaign_id='$campaign';"
+sql5="select list_id from vicidial_lists where campaign_id='$campaign' and active='Y';"
+sql6="select campaign_name from vicidial_campaigns where campaign_id='$campaign';"
 
-campaign_lists=$( mysql --host=$host -u internalreports -p$pw -Dasterisk -Ns -e "$sql4" ) 
+
+campaign_lists=$( mysql --host=$host -u internalreports -p$pw -Dasterisk -Ns -e "$sql5" ) 
 for i in "${campaign_lists[@]}"; do lists+=($i); done
 
 leads_day=0
 leads_sales=0
 leads_contacts=0
+total_login=0
 
 for i in ${lists[@]}; do
 	echo "lista $i"
 	list=$i
 	
-	sql1_func
-	sql2_func
-	sql3_func
+	sql_func
 	
 	day=$( mysql --host=$host -u internalreports -p$pw -Dasterisk -Ns -e "$sql1" )
 	sales=$( mysql --host=$host -u internalreports -p$pw -Dasterisk -Ns -e "$sql2" )
 	contacts=$( mysql --host=$host -u internalreports -p$pw -Dasterisk -Ns -e "$sql3" )
-
+	login=$( mysql --host=$host -u internalreports -p$pw -Dasterisk -Ns -e "$sql4" )
+	
 	let leads_day=$leads_day+$day
 	let leads_sales=$leads_sales+$sales
 	let leads_contacts=$leads_contacts+$contacts
+	let total_login=$total_login+$login
 done
 
-campaign_name=$( mysql --host=$host -u internalreports -p$pw -Dasterisk -Ns -e "$sql5" )
+campaign_name=$( mysql --host=$host -u internalreports -p$pw -Dasterisk -Ns -e "$sql6" )
 
 if [ $leads_day -gt 0 ]; then
 	leads_vs_sales=$( echo "scale=2; $leads_sales*100/$leads_day" | bc | sed 's/^\./0./' )
@@ -83,6 +83,13 @@ else
 	contacts_vs_sales="Not contacts today"
 fi
 
+if [ $total_login -gt 3600 ]; then
+	total_login=$( echo "$total_login/3600" | bc )
+	total_login_vs_sales=$( echo "scale=2; $leads_sales*100/$total_login" | bc | sed 's/^\./0./' )
+else
+	contacts_vs_sales="Not users logins today"
+fi
+
 echo "<html><head></head><body>
 <img src="https://i.imgur.com/RwcasoT.png" alt="logo TN"><br/>
 <b>Proyect:</b> $campaign_name<br/>
@@ -91,7 +98,8 @@ echo "<html><head></head><body>
 <b>Sales =</b> $leads_sales<br/>
 <b>Contacts $fechaI =</b> $leads_contacts<br/>
 <b>Leads vs Sales(%) =</b> $leads_vs_sales<br/>
-<b>Contacts vs Sales(%) =</b> $contacts_vs_sales<br/><br/><br/>
+<b>Contacts vs Sales(%) =</b> $contacts_vs_sales<br/>
+<b>Total Login vs Sales(%) =</b> $total_login_vs_sales<br/><br/><br/>
 This is an automatic report, please don't reply this email.
 </body></html>" > /tmp/$name.html  
 
